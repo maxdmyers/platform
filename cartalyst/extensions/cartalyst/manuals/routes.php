a5be300e7b497af9be299363d71d903b04431cb7
Ben Corlett <bencorlett@me.com>
2012-05-28 16:53:53 +1000

    More logic tidying.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- cartalyst/extensions/cartalyst/manuals/routes.php
+++ cartalyst/extensions/cartalyst/manuals/routes.php
@@ -32,20 +32,24 @@ Route::get('manuals/edit/(:any?)/(:any?)/(:any?)', function($manual, $chapter, $
 // Reading a manual
 Route::get(array('manuals/(:any?)', 'manuals/(:any?)/(:any?)'), function($manual, $chapter = 'introduction')
 {
-	// Get all chapters for the manual / chapter
+	// Get all table of contents' for the manual / chapter
 	$toc         = Manual::toc($manual);
 	$chapter_toc = Manual::chapter_toc($manual, $chapter);
 
+	// Array of articles for the current chapter. The closure
+	// must return either an instance of View or a string which
+	// will be used as the article content.
 	$articles = Manual::articles($manual, $chapter, $chapter_toc, function($article, $manual, $chapter, $article_name)
 	{
-		return View::make('manuals::article')
+		return View::make('manuals::chapter.article')
 		           ->with('article', Manual::parse($article))
 		           ->with('manual', $manual)
 		           ->with('chapter', $chapter)
 		           ->with('article_name', $article_name);
 	});
 
-	return View::make('manuals::view')
+	// Return the chapter page for a manual.
+	return View::make('manuals::chapter')
 	           ->with('toc', Manual::parse($toc))
 	           ->with('chapter_toc', Manual::parse($chapter_toc))
 	           ->with('articles', $articles);
