1b18d1a3a8ce2e0b51d51e37dc07c65427b41e8f
drsii <dan.syme@gmail.com>
2012-06-07 15:30:38 -0700

    installer fix, added url.js and updated installer.js
    Signed-off-by: drsii <dan.syme@gmail.com>

---
+++ public/platform/installer/js/url.js
@@ -0,0 +1,42 @@
+// Just incase the files get loaded out of order
+if (platform == undefined)
+{
+	var platform = { };
+}
+
+// start helpers
+(function($) {
+	// define Helpers object
+	platform.url = {
+
+		urls : {
+			base: null,
+			admin: null,
+		},
+
+		init: function() {
+			this.urls.base  = $('meta[name=base_url]').attr('content');
+			this.urls.admin = $('meta[name=admin_url]').attr('content');
+		},
+
+		base: function(path) {
+			if (path === undefined) {
+				path = ''
+			}
+
+			return this.urls.base + '/' + path;
+		},
+
+		admin: function(path) {
+			if (path === undefined) {
+				path = ''
+			}
+
+			return this.urls.admin + '/' + path;
+		}
+
+	};
+
+	platform.url.init();
+
+})(jQuery);
