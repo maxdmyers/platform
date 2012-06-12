17be5ece5afebcd9c42233b116928fb8dbd9b7ee
drsii <dan.syme@gmail.com>
2012-06-12 15:34:37 -0700

    removed extra div.
    Signed-off-by: drsii <dan.syme@gmail.com>

--- public/platform/themes/backend/default/extensions/themes/index.blade.php
+++ public/platform/themes/backend/default/extensions/themes/index.blade.php
@@ -16,59 +16,58 @@
 
 <section id="themes">
 
-		<header class="row">
-			<div class="span4">
-				<h1>{{ Lang::line('themes::themes.general.title') }}</h1>
-				<p>{{ Lang::line('themes::themes.general.description') }}</p>
-			</div>
-			<nav class="span8">
-				<div class="pull-right">
-					@widget('platform.menus::menus.pills', 'themes')
-				</div>
-			</nav>
-		</header>
-
-		<hr>
-
-		<div class="selections row">
-			@if($active)
-				<div class="active span3">
-					<div class="thumbnail">
-						<img src="{{ Theme::asset('../../'.$active['dir'].'/assets/img/theme-thumbnail.png') }}" title="{{ $active['dir'] }}">
-						<div class="caption">
-							<h5>{{ $active['name'] }}</h5>
-							<p class="version">{{ Lang::line('themes::themes.general.version') }} {{ $active['version'] }}</p>
-							<p class="author">{{ Lang::line('themes::themes.general.author') }}  {{ $active['author'] }}</p>
-							<p>{{ $active['description'] }}</p>
-							<a href="edit/{{ URI::segment(3).'/'.$active['dir'] }}" class="btn" data-theme="{{ $active['dir'] }}" data-type="backend">Edit</a>
-						</div>
-					</div>
-				</div>
-			@else
-				<div class="active span3">
-					<div class="thumbnail">
-						<div class="caption">
-							<h5>Select a Theme and activate</h5>
-						</div>
-					</div>
-				</div>
-			@endif
-
-			@foreach ($inactive as $theme)
-			<div class="span3">
-				<div class="thumbnail inactive">
-					<img src="{{ Theme::asset('../../'.$theme['dir'].'/assets/img/theme-thumbnail.png') }}" title="{{ $theme['dir'] }}">
-						<div class="caption">
-							<h5>{{ $theme['name'] }}</h5>
-							<p>{{ $theme['description'] }}</p>
-							<p>{{ Lang::line('themes::themes.general.version') }} {{ $theme['version'] }}</p>
-							<p>{{ Lang::line('themes::themes.general.author') }}  {{ $theme['author'] }}</p>
-							<a href="activate/{{ URI::segment(3).'/'.$theme['dir'] }}" class="btn activate" data-theme="{{ $theme['dir'] }}" data-type="backend">Activate</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			@endforeach
+	<header class="row">
+		<div class="span4">
+			<h1>{{ Lang::line('themes::themes.general.title') }}</h1>
+			<p>{{ Lang::line('themes::themes.general.description') }}</p>
 		</div>
+		<nav class="span8">
+			<div class="pull-right">
+				@widget('platform.menus::menus.pills', 'themes')
+			</div>
+		</nav>
+	</header>
 
+	<hr>
+
+	<div class="selections row">
+		@if($active)
+			<div class="active span3">
+				<div class="thumbnail">
+					<img src="{{ Theme::asset('../../'.$active['dir'].'/assets/img/theme-thumbnail.png') }}" title="{{ $active['dir'] }}">
+					<div class="caption">
+						<h5>{{ $active['name'] }}</h5>
+						<p class="version">{{ Lang::line('themes::themes.general.version') }} {{ $active['version'] }}</p>
+						<p class="author">{{ Lang::line('themes::themes.general.author') }}  {{ $active['author'] }}</p>
+						<p>{{ $active['description'] }}</p>
+						<a href="edit/{{ URI::segment(3).'/'.$active['dir'] }}" class="btn" data-theme="{{ $active['dir'] }}" data-type="backend">Edit</a>
+					</div>
+				</div>
+			</div>
+		@else
+			<div class="active span3">
+				<div class="thumbnail">
+					<div class="caption">
+						<h5>Select a Theme and activate</h5>
+					</div>
+				</div>
+			</div>
+		@endif
+
+		@foreach ($inactive as $theme)
+		<div class="span3">
+			<div class="thumbnail inactive">
+				<img src="{{ Theme::asset('../../'.$theme['dir'].'/assets/img/theme-thumbnail.png') }}" title="{{ $theme['dir'] }}">
+				<div class="caption">
+					<h5>{{ $theme['name'] }}</h5>
+					<p>{{ $theme['description'] }}</p>
+					<p>{{ Lang::line('themes::themes.general.version') }} {{ $theme['version'] }}</p>
+					<p>{{ Lang::line('themes::themes.general.author') }}  {{ $theme['author'] }}</p>
+					<a href="activate/{{ URI::segment(3).'/'.$theme['dir'] }}" class="btn activate" data-theme="{{ $theme['dir'] }}" data-type="backend">Activate</a>
+				</div>
+			</div>
+		</div>
+		@endforeach
+	</div>
+</section>
 @endsection
