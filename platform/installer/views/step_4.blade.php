6c768c13ce1e44854b90b64a263e9125c53a1733
drsii <dan.syme@gmail.com>
2012-06-13 14:43:08 -0700

    revamped installer design to be inline with default backend template.
    Signed-off-by: drsii <dan.syme@gmail.com>

--- platform/installer/views/step_4.blade.php
+++ platform/installer/views/step_4.blade.php
@@ -2,20 +2,26 @@
 
 @section('content')
 <div id="installer">
+	<div class="content">
 
-	<div class="well">
 		<div class="brand">
 			{{ HTML::image('platform/installer/img/brand.png', 'Platform by Cartalyst'); }}
 		</div>
+		<h1>All Done :)</h1>
+		<p>Congratulations - you've installed Platform!</p>
 
-		<div class="step row-fluid">
-			<div class="finish span12">
-				<h1>Finished!</h1>
+		<div class="breadcrumbs">
+			<ul class="nav">
+				<li>Step 1</li>
+				<li>Step 2</li>
+				<li>Step 3</li>
+				<li class="active">Finish</li>
+			</ul>
+		</div>
+		<div class="finish">
 
-				<p>Congratulations - you've installed Platform!</p>
+			<a href="{{ url(ADMIN) }}" class="btn btn-primary">Login to Admin</a>
 
-				<a href="{{ url(ADMIN) }}" class="btn btn-primary">Login to Admin</a>
-			</div>
 		</div>
 	</div>
 </div>
