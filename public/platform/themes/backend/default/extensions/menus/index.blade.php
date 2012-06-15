38395de323f92a9ed818d4940e8cbac3d370d087
Ben Corlett <bencorlett@me.com>
2012-06-16 09:58:59 +1000

    Tidying up the menu table.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- public/platform/themes/backend/default/extensions/menus/index.blade.php
+++ public/platform/themes/backend/default/extensions/menus/index.blade.php
@@ -16,10 +16,41 @@
 	{{ HTML::link('admin/menus/create', 'Create New Menu', array('class' => 'btn btn-primary pull-right')) }}
 </div>
 
+<table class="table table-bordered">
+	<thead>
+		<tr>
+			<th>Menu</th>
+			<th></th>
+		</tr>
+	</thead>
+	<tbody>
+		@forelse ($menus as $menu)
+			<tr>
+				<td>
+					{{ $menu['name'] }}
+				</td>
+				<td>
+					{{ HTML::link('admin/menus/edit/'.$menu['id'], 'Edit', array('class' => 'btn')) }}
+
+					@if ($menu['user_editable'])
+						{{ HTML::link('admin/menus/delete'.$menu['id'], 'Delete', array('class' => 'btn btn-danger', 'onclick' => 'return confirm(\'Are you sure you want to delete this menu? This cannot be undone.\');')) }}
+					@endif
+				</td>
+			</tr>
+		@empty
+			<tr colspan="2">
+				<td>
+					No menus yet.
+				</td>
+			</tr>
+		@endforelse
+	</tbody>
+</table>
+
 <ul class="nav nav-tabs nav-stacked">
 	@foreach ($menus as $menu)
 		<li>
-			{{ HTML::link('admin/menus/edit/'.$menu['id'], "'{$menu['name']}' Menu") }}
+			
 		</li>
 	@endforeach
 </ul>
