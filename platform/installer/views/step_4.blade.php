0cec6e60e8c51b437de68d1b9936dec792d0be4f
drsii <dan.syme@gmail.com>
2012-06-16 19:10:00 -0700

    updated installation complete page.
    Signed-off-by: drsii <dan.syme@gmail.com>

--- platform/installer/views/step_4.blade.php
+++ platform/installer/views/step_4.blade.php
@@ -7,20 +7,14 @@
 		<div class="brand">
 			{{ HTML::image('platform/installer/img/brand.png', 'Platform by Cartalyst'); }}
 		</div>
-		<h1>All Done :)</h1>
-		<p>Congratulations - you've installed Platform!</p>
+		<h1>Installation Complete</h1>
 
-		<div class="breadcrumbs">
-			<ul class="nav">
-				<li>Step 1</li>
-				<li>Step 2</li>
-				<li>Step 3</li>
-				<li class="active">Finish</li>
-			</ul>
-		</div>
+		<p class="lead">Congratulations - you've installed Platform! <strong>But wait! Not so fast</strong>, we highly recommend you delete the following folders now that you have completed the installation process.</p>
+		<pre><code>platform/installer/*</code></pre>
+		<pre><code>public/installer/*</code></pre>
+
 		<div class="finish">
-
-			<a href="{{ url(ADMIN) }}" class="btn btn-primary">Login to Admin</a>
+			<p><a href="{{ url(ADMIN) }}" class="btn btn-large">Login to Admin</a> <a href="{{ URL::base() }}" class="btn btn-large">Or visit the Home Page</a></p>
 
 		</div>
 	</div>
