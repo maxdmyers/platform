711d91825923bd90cb13a3b179a2ad6f4e759606
drsii <dan.syme@gmail.com>
2012-06-26 10:11:52 -0700

    removing ispum.
    Signed-off-by: drsii <dan.syme@gmail.com>

--- public/platform/themes/backend/default/extensions/dashboard/index.blade.php
+++ public/platform/themes/backend/default/extensions/dashboard/index.blade.php
@@ -2,12 +2,4 @@
 
 @section ('content')
 
-<p>Aenean lacinia bibendum nulla sed consectetur. Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Donec sed odio dui.</p>
-
-<p>Donec sed odio dui. Donec sed odio dui. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui. Cras mattis consectetur purus sit amet fermentum.</p>
-
-<p>Nulla vitae elit libero, a pharetra augue. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Maecenas faucibus mollis interdum. Donec sed odio dui. Nulla vitae elit libero, a pharetra augue.</p>
-
-<p>Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vestibulum id ligula porta felis euismod semper. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper.</p>
-
 @endsection
